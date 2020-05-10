150b480698a5b943d72f98199a892eeb911f3f02
Melisa <melisosa@alumno.unlam.edu.ar>
2020-05-10 16:45:57 -0300

    Inicio repositorio Sisop

---
+++ Powershell/1/ejercicio1.ps1
@@ -0,0 +1,28 @@
+﻿Param (
+    [Parameter(Position = 1, Mandatory = $false)]
+    [String] $pathsalida = ".\procesos.txt ",
+    [int] $cantidad = 3
+)
+
+$existe = Test-Path $pathsalida
+
+if ($existe -eq $true) {
+    $listaproceso = Get-Process
+    foreach ($proceso in $listaproceso) {
+        $proceso | Format-List -Property Id,Name >> $pathsalida
+    }
+    for ($i = 0; $i -lt $cantidad ; $i++) {
+        Write-Host $listaproceso[$i].Name - $listaproceso[$i].Id
+    }
+} else {
+    Write-Host "El path no existe"
+}
+
+<#
+    1. El script escribe en un archivo que recibe como parámetro ($pathsalida) los procesos del sistema, detallando id y nombre.
+       Después muestra por pantalla la cantidad de procesos que recibe como parámetro ($cantidad).
+    2. Se podría validar que el parámetro $pathsalida, además de existir, sea un archivo.
+    3. Si se ejecuta el script sin ningun parámetro se le van a asignar los valores por defecto que son:
+        Para el parámetro pathsalida -> ".\procesos.txt"
+        Para el parámetro cantidad -> 3
+#>
---
+++ Powershell/2/procesarLogs.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+Procesamiento de logs semanales
+
+.DESCRIPTION
+Este script recorre el directorio de logs semanales y procesa los de cada semana mostrando los siguientes resultados:
+▪ Promedio de tiempo de las llamadas realizadas por día.
+▪ Promedio de tiempo y cantidad por usuario por día.
+▪ Los 3 usuarios con más llamadas en la semana.
+▪ Cuántas llamadas no superan la media de tiempo por día y el usuario que tiene más llamadas por debajo de la media en la semana.
+
+.PARAMETER Path
+Directorio en el que se encuentran los archivos de logs
+
+.EXAMPLE
+procesarLogs.ps1 -Path /logs/
+#>
+
+class Log {
+
+    [String]$username;
+    [datetime]$fecha;
+    [datetime]$hora;
+   
+}
+
+$lines = Get-Content -Path $args[0]
+
+for($i=0; $i -le $lines.Length; $i++) {
+    $log = Get-Content -Path $lines -Delimiter " "
+
+    Write-Host $log[0] `n
+    Write-Host $log[1] `n
+    Write-Host $log[2] `n
+    Write-Host $log[3] `n
+    break
+}
