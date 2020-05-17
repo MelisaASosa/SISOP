e12fa2d917d83d6f5ec9a23ee82339cd5f50b18c
Melisa <melisosa@alumno.unlam.edu.ar>
2020-05-17 00:43:38 -0300

    Resolución ejercicio 2 (sin Format-List)

--- Powershell/2/procesarLogs.ps1
+++ Powershell/2/procesarLogs.ps1
@@ -13,25 +13,161 @@ Este script recorre el directorio de logs semanales y procesa los de cada semana
 Directorio en el que se encuentran los archivos de logs
 
 .EXAMPLE
-procesarLogs.ps1 -Path /logs/
+.\procesarLogs.ps1 -Path /logs/
 #>
 
-class Log {
+Param(
+    [Parameter(Mandatory=$true)]
+    $directorio
+)
 
-    [String]$username;
-    [datetime]$fecha;
-    [datetime]$hora;
-   
+$resultados = @{}
+
+## FUNCIONES ##
+function mostrarResultadosDiarios {
+    Param(
+        [DateTime] $dia
+    )
+
+    $acumuladoTiempoDiario = ($acumuladoTiempoPorUsuarioPorDia.Values | Measure-Object -Sum).Sum
+    $cantidadDiaria = ($cantidadLlamadasPorUsuarioPorDia.Values | Measure-Object -Sum).Sum
+
+    $promedio = [timespan]::fromseconds($acumuladoTiempoDiario / $cantidadDiaria)
+    
+    Write-Host "-------------  " $dia.ToShortDateString() "  --------------"
+    Write-Host "El promedio de tiempo de las llamadas es: $promedio `n"
+
+    foreach ($cantidadUsuario in $cantidadLlamadasPorUsuarioPorDia.GetEnumerator()) {
+        $usuario = $cantidadUsuario.Name
+        $cantidad = $cantidadUsuario.Value
+        $acumulado = @($acumuladoTiempoPorUsuarioPorDia.GetEnumerator() | Where-Object {$_.Name -eq $usuario}).Value
+
+        $promedioUsuario = [timespan]::fromseconds($acumulado / $cantidad)
+
+        Write-Host "La cantidad de llamadas de $usuario en el día es de: $cantidad"
+		Write-Host "El promedio de tiempo de las llamadas de $usuario en el día es de: $promedioUsuario `n"
+    }
+
+    $cantidadLlamadasMenorPromedio = 0
+
+    foreach($tiempo in $tiemposLlamadasPorDia.GetEnumerator()) {
+        if($tiempo.Value -lt $promedio) {
+            $cantidadLlamadasMenorPromedio++
+        }
+    }
+
+    Write-Host "La cantidad de llamadas que no superan la media de tiempo ($promedio) es: $cantidadLlamadasMenorPromedio `n`n"
 }
 
-$lines = Get-Content -Path $args[0]
+function mostrarResultadosSemanales {
+    Param(
+        $infoLog
+    )
 
-for($i=0; $i -le $lines.Length; $i++) {
-    $log = Get-Content -Path $lines -Delimiter " "
+    $cantidadLlamadasPorUsuarioPorSemana = $cantidadLlamadasPorUsuarioPorSemana.GetEnumerator() | Sort-Object -Property Value -Descending
 
-    Write-Host $log[0] `n
-    Write-Host $log[1] `n
-    Write-Host $log[2] `n
-    Write-Host $log[3] `n
-    break
+    Write-Host "Los 3 usuarios con más llamadas en la semana son: "
+    Write-Host "1." $cantidadLlamadasPorUsuarioPorSemana[0].Name ":" $cantidadLlamadasPorUsuarioPorSemana[0].Value "llamadas"
+    Write-Host "1." $cantidadLlamadasPorUsuarioPorSemana[1].Name ":" $cantidadLlamadasPorUsuarioPorSemana[1].Value "llamadas"
+    Write-Host "1." $cantidadLlamadasPorUsuarioPorSemana[2].Name ":" $cantidadLlamadasPorUsuarioPorSemana[2].Value "llamadas`n"
+
+    #Calculo promedio semanal
+    
+    $tiempoTotalSemanal = ($acumuladoTiempoPorUsuarioPorSemana.Values | Measure-Object -Sum).Sum
+    $cantidadTotalSemanal = ($cantidadLlamadasPorUsuarioPorSemana.Value | Measure-Object -Sum).Sum
+
+    $promedioSemanal = [timespan]::fromseconds($tiempoTotalSemanal / $cantidadTotalSemanal)
+
+    $cantidadDebajoMediaPorUsuario = @{}
+
+    for($i = 0; $i -lt $infoLog.Count; $i++) {
+        $usuario = $infoLog[$i].Usuario
+
+        if($horariosInicioPorUsuario.$usuario -ne $null) {
+            $tiempoLlamada = [DateTime]$infoLog[$i].Hora - [DateTime]$horariosInicioPorUsuario.$usuario
+            $horariosInicioPorUsuario.$usuario = $null
+
+            if($tiempoLlamada -lt $promedioSemanal) {
+                $cantidadDebajoMediaPorUsuario.$usuario++
+            }
+        } else {
+            $horariosInicioPorUsuario.$usuario = $infoLog[$i].Hora
+        }
+    }
+
+    $cantidadDebajoMediaPorUsuario = $cantidadDebajoMediaPorUsuario.GetEnumerator() | Sort-Object -Property Value -Descending
+
+    Write-Host "El usuario con más llamadas por debajo de la media semanal es: " $cantidadDebajoMediaPorUsuario[0].Name "(" $cantidadDebajoMediaPorUsuario[0].Value "llamadas )"
+
 }
+## END FUNCIONES ##
+
+## MAIN ##
+
+#Valido existencia de directorio
+$Existe = Test-Path $directorio
+
+if(!$Existe) {
+    Write-Error "El directorio no existe"
+    Exit
+}
+
+#Recorro el directorio de logs para procesar todos los logs existentes.
+$semana = 1
+foreach($logFile in Get-ChildItem $directorio) {
+
+    $infoLog = Import-Csv -Path $logFile -Header 'Fecha', 'Hora', 'Guion', 'Usuario' -Delimiter " "
+    $infoLog =  $infoLog | Select-Object -Property Fecha, Hora, Usuario | Sort-Object Fecha, Hora
+
+    $diaEnProceso = [DateTime]$infoLog[0].Fecha
+
+    $cantidadLlamadasPorUsuarioPorDia = @{}
+    $cantidadLlamadasPorUsuarioPorSemana = @{}
+    $horariosInicioPorUsuario = @{}
+    $acumuladoTiempoPorUsuarioPorDia = @{}
+    $acumuladoTiempoPorUsuarioPorSemana = @{}
+    $tiemposLlamadasPorDia = @{}
+
+    $cont = 0
+
+    Write-Host "*************  RESULTADOS SEMANA $semana  ************* `n"
+
+    for($i = 0; $i -lt $infoLog.Count; $i++) {
+        $diaRegistro = [DateTime]$infoLog[$i].Fecha
+        $usuario = $infoLog[$i].Usuario
+
+        if($diaEnProceso -ne $diaRegistro) {
+            mostrarResultadosDiarios $diaEnProceso $cantidadLlamadasDiarias
+            $cont=0
+		    $cantidadLlamadasPorUsuarioPorDia = @{}
+            $acumuladoTiempoPorUsuarioPorDia = @{}
+        }
+        
+        if($horariosInicioPorUsuario.$usuario -ne $null) {
+            $tiempoLlamada = [DateTime]$infoLog[$i].Hora - [DateTime]$horariosInicioPorUsuario.$usuario
+
+            $acumuladoTiempoPorUsuarioPorDia.$usuario = $acumuladoTiempoPorUsuarioPorDia.$usuario + $tiempoLlamada.TotalSeconds
+            $acumuladoTiempoPorUsuarioPorSemana.$usuario = $acumuladoTiempoPorUsuarioPorSemana.$usuario + $tiempoLlamada.TotalSeconds
+            $tiemposLlamadasPorDia.$cont = $tiempoLlamada
+
+            $cont++
+
+            $horariosInicioPorUsuario.$usuario = $null
+        } else {
+            $cantidadLlamadasPorUsuarioPorDia.$usuario++
+            $cantidadLlamadasPorUsuarioPorSemana.$usuario++
+            $horariosInicioPorUsuario.$usuario = $infoLog[$i].Hora
+        }
+
+        $diaEnProceso = [DateTime]$infoLog[$i].Fecha
+    }
+
+    mostrarResultadosDiarios $diaEnProceso $cantidadLlamadasDiarias
+
+    mostrarResultadosSemanales $infoLog
+
+    Write-Host "*************  FIN RESULTADOS SEMANA $semana  ************* `n"
+
+    $semana++
+}
+## FIN MAIN ##
